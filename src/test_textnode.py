2e329fe4754ae04ff3ff40a9ff7ec90f12e4c0cb
TheRealFezbot <ferryrietveld@live.nl>
2025-12-13 16:02:44 +0200

    Added testcases to test textnode.py

---
+++ src/test_textnode.py
@@ -0,0 +1,34 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+    
+    def test_not_eq(self):
+        node = TextNode("This is a bold node", TextType.BOLD)
+        node2 = TextNode("This is a bold node", TextType.TEXT)
+        self.assertNotEqual(node, node2)
+
+    def test_not_eq2(self):
+        node = TextNode("This is a bold node", TextType.TEXT)
+        node2 = TextNode("This is a text node", TextType.TEXT)
+        self.assertNotEqual(node, node2)
+
+    def test_url_eq(self):
+        node = TextNode("This is a link", TextType.LINK, "https://boot.dev")
+        node2 = TextNode("This is a link", TextType.LINK, "https://boot.dev")
+        self.assertEqual(node, node2)
+    
+    def test_url_not_eq(self):
+        node = TextNode("This is a link", TextType.LINK, "https://boot.dev")
+        node2 = TextNode("This is a link", TextType.LINK, "https://boot.com")
+        self.assertNotEqual(node, node2)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
